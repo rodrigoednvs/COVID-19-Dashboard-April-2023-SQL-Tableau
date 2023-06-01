21827bdaf937d69736623e58aa25a4422f90a4bd
Rodrigo Neves <130403759+rodrigoednvs@users.noreply.github.com>
2023-06-01 16:04:43 -0300

    COVID SQL.sql

--- COVID SQL.sql
+++ COVID SQL.sql
@@ -1,129 +1,48 @@
-SELECT *
+/*
+
+Consultas usadas para o projeto Tableau Dashboard COVID-19 - Abril 2023
+https://public.tableau.com/app/profile/rodrigonvs/viz/COVID-19Dashboard-Abril2023/COVID-192023
+*/
+
+-- 1. Total de Casos Globais, Total de Mortes e Porcentagem de Mortes Globais
+
+SELECT 
+  SUM(new_cases) AS total_cases, 
+  SUM(new_deaths) AS total_deaths, 
+  ROUND(SUM(new_deaths) / NULLIF(SUM(new_cases), 0)*100, 2) AS death_percentage
 FROM `portifolio-project-05042023.covid_dataset.covid_deaths`
 WHERE continent IS NOT NULL
-ORDER BY 3,4
-
--- Looking at Total Cases vs Total Deaths
--- Shows likelehood of diying if you contract Covid in Brazil
-
-SELECT location, date, total_cases, total_deaths, (total_deaths / total_cases)*100 AS death_pct
-FROM `portifolio-project-05042023.covid_dataset.covid_deaths`
-WHERE location = 'Brazil' 
-AND continent IS NOT NULL
-ORDER BY 1,2 DESC
-
-
--- looking at Total Cases vs Population
--- Shows what percentage of population got Covid
-
-SELECT location, date, population, total_cases, (total_cases / population)*100 AS cases_pct
-FROM `portifolio-project-05042023.covid_dataset.covid_deaths`
-WHERE location = 'Brazil'
-AND continent IS NOT NULL
 ORDER BY 1,2
 
--- Looking at countries with Highest Infection Rate compared to its Population
+-- 2. Total de Mortes por Continente
 
-SELECT location, population, MAX(total_cases) AS highest_infection_cases, ROUND(MAX((total_cases / population))*100, 2) AS pop_infected_pct
+SELECT 
+  location, SUM(new_deaths) as TotalDeathCount
 FROM `portifolio-project-05042023.covid_dataset.covid_deaths`
-WHERE continent IS NOT NULL
-GROUP BY location, population
-ORDER BY pop_infected_pct DESC
-
--- Showing countries with Highes Death Count per Population
-
-SELECT location, MAX(total_deaths) AS total_deaths_count 
-FROM `portifolio-project-05042023.covid_dataset.covid_deaths`
-WHERE continent IS NOT NULL
+WHERE continent is null 
+AND location IN ('Europe', 'Asia', 'North America', 'South America', 'Africa', 'Oceania')
 GROUP BY location
-ORDER BY total_deaths_count DESC
+ORDER BY TotalDeathCount DESC
 
--- Breaking down by continents
--- Showing the continents with Highest Death Count per Population
-
-SELECT continent, MAX(total_deaths) AS total_deaths_count 
-FROM `portifolio-project-05042023.covid_dataset.covid_deaths`
-WHERE continent IS NOT NULL
-GROUP BY continent
-ORDER BY total_deaths_count DESC
-
-
--- GLOBAL NUMBERS
+-- 3. Porcentagem de Infectados por País
 
 SELECT 
+  Location, 
+  Population, 
+  MAX(total_cases) AS HighestInfectionCount,  
+  ROUND(MAX((total_cases/population))*100, 2) AS PercentPopulationInfected
+FROM `portifolio-project-05042023.covid_dataset.covid_deaths`
+GROUP BY Location, Population
+ORDER BY PercentPopulationInfected DESC
+
+-- 4. Porcentagem de Infectados por País - Mensal
+
+SELECT 
+  Location, 
+  Population, 
   date, 
-  SUM(new_cases) AS total_cases_day, 
-  SUM(new_deaths) AS total_deaths_day, 
-  ROUND(SUM(new_deaths) / NULLIF(SUM(new_cases), 0)*100, 2) AS death_pct_day
+  MAX(total_cases) as HighestInfectionCount, 
+  ROUND(MAX((total_cases/population))*100, 2) as PercentPopulationInfected
 FROM `portifolio-project-05042023.covid_dataset.covid_deaths`
-WHERE continent IS NOT NULL
-GROUP BY date
-ORDER BY 1,2
-
--- Looking ant Total Population vs Vaccinations
-
-SELECT 
-  dea.continent, 
-  dea.location, 
-  dea.date, 
-  dea.population, 
-  vac.new_vaccinations, 
-  SUM(vac.new_vaccinations) OVER (
-    PARTITION BY dea.location 
-    ORDER BY dea.location, dea.date
-    ) AS rolling_ppl_vac,
-  (rolling_ppl_vac / population)
-FROM `portifolio-project-05042023.covid_dataset.covid_deaths` AS dea
-JOIN `portifolio-project-05042023.covid_dataset.covid_vaccinations` AS vac
-  ON dea.location = vac.location
-  AND dea.date = vac.date
-WHERE dea.continent IS NOT NULL
-ORDER BY 2,3
-
-
--- USE CTE
-
-WITH PplvsVac 
-AS 
-(
-  SELECT 
-  dea.continent, 
-  dea.location, 
-  dea.date, 
-  dea.population, 
-  vac.new_vaccinations, 
-  SUM(vac.new_vaccinations) OVER (
-    PARTITION BY dea.location 
-    ORDER BY dea.location, dea.date
-    ) AS rolling_ppl_fully_vac
-  FROM `portifolio-project-05042023.covid_dataset.covid_deaths` AS dea
-  JOIN `portifolio-project-05042023.covid_dataset.covid_vaccinations` AS vac
-    ON dea.location = vac.location
-    AND dea.date = vac.date
-  WHERE dea.continent IS NOT NULL
-)
-SELECT *, ROUND((rolling_ppl_fully_vac / population)*100, 2) AS rolling_ppl_fully_vac_pctg
-FROM PplvsVac
-ORDER BY 2,3
-
-
--- Creating view to store date for later visualizations
-
-CREATE VIEW `portifolio-project-05042023.covid_dataset.ppl_vaccinated_pctg` AS
-SELECT 
-  dea.continent, 
-  dea.location, 
-  dea.date, 
-  dea.population, 
-  vac.new_vaccinations, 
-  SUM(vac.new_vaccinations) OVER (
-    PARTITION BY dea.location 
-    ORDER BY dea.location, dea.date
-    ) AS rolling_ppl_fully_vac
-  FROM `portifolio-project-05042023.covid_dataset.covid_deaths` AS dea
-  JOIN `portifolio-project-05042023.covid_dataset.covid_vaccinations` AS vac
-    ON dea.location = vac.location
-    AND dea.date = vac.date
-  WHERE dea.continent IS NOT NULL
-
-  
+GROUP BY Location, Population, date
+ORDER BY PercentPopulationInfected DESC
